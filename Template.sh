13d422022908b0a448dc105946c1462818e8fb86
joshua <joshuaglennmurphy@gmail.com>
2016-01-11 15:21:11 +1300

    Create Template.sh

---
+++ Template.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Script to INSERT PURPOSE
+# By Josh
+# Version .01 "INSERT MEMORABLE EVENT"
+
+
+# todo:
+
+
+
+
+
+
+# Variables
+
+
+
+
+
+
+# Functions
+
+
+
+
+
+
+# Actual code
+
+
+
+
+
+
+
+
+
+
+
+exit
